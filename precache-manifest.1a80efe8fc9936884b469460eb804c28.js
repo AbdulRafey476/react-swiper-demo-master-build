ee22526259eb944d2e45b4476801178b3b443962
abdulrafay476 <abdul.rafey476@hotmail.com>
2022-06-13 21:51:04 +0500

    init

---
+++ precache-manifest.1a80efe8fc9936884b469460eb804c28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef7ddf9eba40bcf9ea3c8a57af48061",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b25e3f07908de75c968f",
+    "url": "/static/css/2.a384d093.chunk.css"
+  },
+  {
+    "revision": "ee1a0f692bf6a019562c",
+    "url": "/static/css/main.4c7b4d22.chunk.css"
+  },
+  {
+    "revision": "b25e3f07908de75c968f",
+    "url": "/static/js/2.3bd25023.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3bd25023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1a0f692bf6a019562c",
+    "url": "/static/js/main.ca7be128.chunk.js"
+  },
+  {
+    "revision": "960fbdc7b0ba4132bf91",
+    "url": "/static/js/runtime-main.bbf4945e.js"
+  }
+]);
